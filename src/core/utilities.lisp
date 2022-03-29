013b5b7264b0f302f27de6f19ce7c6501ca89ebb
Stewart V. Wright <stewart@vifortech.com>
2022-03-29 11:44:43 +1100

    Allow using previously generated enumeration lists

--- src/core/utilities.lisp
+++ src/core/utilities.lisp
@@ -10,13 +10,30 @@
 
 ;; Enumeration - useful for plotting
 
-(defun enumerate (l &key (test #'eql))
-  "Returns an alist mapping unique items in L to integers"
-  (let ((uniq (remove-duplicates l :test test)))
+(defun enumerate (l &key (test #'eql) (enums nil enums-p))
+  "Returns an alist mapping unique items in L to integers
+
+:TEST for key comparison
+:ENUMS to use existing enumeration list"
+  (let ((uniq (remove-duplicates l :test test))
+        (biggest 0))
+    (when enums-p
+      ;; If enums provided, get the list of known-keys and the largest enum
+      ;; number seen, then remove any known-keys from the list of unique keys
+      (multiple-value-bind (known-keys big)
+          (loop for item in enums
+                collect (car item) into known-keys
+                maximize (cdr item) into big
+                minimize (cdr item) into small
+                finally (return (values known-keys big small)))
+        (setf uniq (remove-if (lambda (it) (find it known-keys :test test))
+                              uniq)
+              biggest big)))
+    ;; Add any unique keys to the enum list
     (loop for item in uniq
-          for i from 1
-          ;; collect (cons item (make-symbol (princ-to-string i)))
-          collect (cons item i))))
+          for i from (1+ biggest)
+          do (push (cons item i) enums))
+    enums))
 
 ;; (enumerate '(a b c a))
 
